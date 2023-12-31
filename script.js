f19443f5f50b1affac52839a9e4a334197516a6d
faiz832 <faqodefazz@gmail.com>
2023-12-31 13:39:05 +0700

    cmmt

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ document.addEventListener("DOMContentLoaded", function () {
     navItemGroup.classList.toggle("active");
   });
 });
+
+// === image slider galeri ===
+var indexValue = 1;
+showImg(indexValue);
+function img_slide(e) {
+  showImg((indexValue = e));
+}
+function btn_slide(e) {
+  showImg((indexValue += e));
+}
+function showImg(e) {
+  var i;
+  const img = document.querySelectorAll(".img-galeri");
+  const sliders = document.querySelectorAll(".content-img-nav span");
+  if (e > img.length) {
+    indexValue = 1;
+  }
+  if (e < 1) {
+    indexValue = img.length;
+  }
+  for (i = 0; i < img.length; i++) {
+    img[i].style.display = "none";
+  }
+  for (i = 0; i < sliders.length; i++) {
+    sliders[i].style.filter = "grayscale(100%)";
+  }
+  img[indexValue - 1].style.display = "block";
+  sliders[indexValue - 1].style.filter = "grayscale(0%)";
+}
